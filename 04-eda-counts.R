0f32c18f5fb32a62bb967b1b83ae2a9333d6bf87
jpbisciotti <76615346+jpbisciotti@users.noreply.github.com>
2026-05-23 23:43:47 -0400

    Update 04-eda-counts.R

--- 04-eda-counts.R
+++ 04-eda-counts.R
@@ -1,3 +1,7 @@
+levels_dependent <- c("Y", "N")
+
+levels_predictor <- c("Y", "N")
+
 eda_analysis <- purrr::map(
   role_independent_nominal_predictor,
   ~ oswego |> 
@@ -7,29 +11,57 @@ eda_analysis <- purrr::map(
     ) |>
     dplyr::mutate(predictor = .x) |>
     dplyr::rename(predictor_level = !!rlang::sym(.x)) |>
-    dplyr::select(predictor, predictor_level, ill, n) |>
+    dplyr::mutate(dependent = role_dependent) |>
+    dplyr::rename(dependent_level := !!rlang::sym(role_dependent)) |> 
+    dplyr::select(predictor, predictor_level, dependent, dependent_level, n) |>
     #
     dplyr::rename(n_cell = n) |>
     dplyr::mutate(n_predictor = sum(n_cell), .by = predictor_level) |>
-    dplyr::mutate(n_dependent = sum(n_cell), .by = ill) |>
+    dplyr::mutate(n_dependent = sum(n_cell), .by = dependent_level) |>
     #
     dplyr::mutate(n_cell_ok = all(n_cell[!is.na(predictor_level)] >= 5L)) |>
     dplyr::mutate(n_predictor_ok = all(n_predictor[!is.na(predictor_level)] >= 5L)) |>
     dplyr::mutate(n_dependent_ok = all(n_dependent[!is.na(predictor_level)] >= 10L)) |>
     #
-    dplyr::mutate(all_dependent_levels = all(levels_dependent %in% ill)) |>
+    dplyr::mutate(all_dependent_levels = all(levels_dependent %in% dependent_level)) |>
     dplyr::mutate(all_predictor_levels = all(na.omit(levels_predictor %in% predictor_level))) |>
     dplyr::mutate(all_cell_levels = all_dependent_levels & all_predictor_levels) 
 )
 
 eda_not_all_cell_levels  <- eda_analysis |>
-  purrr::keep(~ !all(dplyr::pull(.x, all_cell_levels)))
+  purrr::keep(~ !all(dplyr::pull(.x, all_cell_levels))) |>
+  purrr::map(
+    ~ .x |> 
+      dplyr::select(dependent, dependent_level, predictor, predictor_level, n_cell) |>
+      janitor::adorn_totals() |>
+      tibble::as_tibble()
+  )
 
 eda_n_dependent_not_ok <- eda_analysis |>
-  purrr::keep(~ !all(dplyr::pull(.x, n_dependent_ok)))
+  purrr::keep(~ !all(dplyr::pull(.x, n_dependent_ok))) |>
+  purrr::map(
+    ~ .x |> 
+      dplyr::select(dependent, dependent_level, n_dependent) |>
+      dplyr::distinct() |>
+      janitor::adorn_totals() |>
+      tibble::as_tibble()
+  )
 
 eda_n_predictor_not_ok <- eda_analysis |>
-  purrr::keep(~ !all(dplyr::pull(.x, n_predictor_ok)))
+  purrr::keep(~ !all(dplyr::pull(.x, n_predictor_ok))) |>
+  purrr::map(
+    ~ .x |> 
+      dplyr::select(predictor, predictor_level, n_predictor) |>
+      dplyr::distinct() |>
+      janitor::adorn_totals() |>
+      tibble::as_tibble()
+  )
 
 eda_n_cell_not_ok <- eda_analysis |>
-  purrr::keep(~ !all(dplyr::pull(.x, n_cell_ok)))
+  purrr::keep(~ !all(dplyr::pull(.x, n_cell_ok))) |> 
+  purrr::map(
+    ~ .x |> 
+      dplyr::select(dependent, dependent_level, predictor, predictor_level, n_cell) |>
+      janitor::adorn_totals() |>
+      tibble::as_tibble()
+  )
