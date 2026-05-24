21211c9d673b46d064fad0677efe6a1b8c2cafda
jpbisciotti <76615346+jpbisciotti@users.noreply.github.com>
2026-05-24 18:30:58 -0400

    Update 03-roles.R

--- 03-roles.R
+++ 03-roles.R
@@ -2,9 +2,26 @@ role_id <- c("person_id")
 
 role_dependent <- c("ill")
 
-role_independent_numeric <- c(
-  "age"
+role_independent_numeric <- c("age")
+
+role_independent_nominal_control <- c("sex")
+
+not_independent_nominal_predictors <- c(
+  role_id, 
+  role_dependent, 
+  role_independent_numeric, 
+  role_independent_nominal_control
 )
+
+if (FALSE) {
+  # Identify independent nominal predictors. Copy console output and paste into
+  # source code. We prefer to define column roles explicitly, rather than just
+  # using the result of setdiff.
+  oswego |>
+    colnames() |>
+    setdiff(not_independent_nominal_predictors) |>
+    dput()
+}
 
 role_independent_nominal_predictor <- c(
   "baked_ham", 
@@ -23,11 +40,9 @@ role_independent_nominal_predictor <- c(
   "fruit_salad"
 )
 
-role_independent_nominal_control <- c(
-  "sex"
-)
-
 role_independent_nominal <- c(
   role_independent_nominal_predictor,
   role_independent_nominal_control
 )
+
+rm(not_independent_nominal_predictors)
